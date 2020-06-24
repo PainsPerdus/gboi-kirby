8c95ccbb8389533d71132e94fa11b413b6617031
Yohaï-Eliel Berreby <yberreby@protonmail.ch>
2020-06-24 16:32:28 +0200

    add C pineapple demo
    Squashed commit of the following:
    
    commit fba96843c3acc681a4705137622c3b8bee6d6f2d
    Author: Yohaï-Eliel Berreby <yberreby@protonmail.ch>
    Date:   Wed Jun 24 16:22:15 2020 +0200
    
        animate all directions
    
    commit 6b88a761781ef25f4812675e0736a525000071b0
    Author: Yohaï-Eliel Berreby <yberreby@protonmail.ch>
    Date:   Wed Jun 24 16:02:15 2020 +0200
    
        dynamic player animation length (wip)
    
    commit f869c5391504400e0ed5f6eb0a3b2fcb0482410b
    Author: Yohaï-Eliel Berreby <yberreby@protonmail.ch>
    Date:   Wed Jun 24 15:59:46 2020 +0200
    
        add new wasteful flipping logic
    
    commit fff5566ab2781d8139e643e8c71312b7d8245eb0
    Author: Yohaï-Eliel Berreby <yberreby@protonmail.ch>
    Date:   Wed Jun 24 15:49:48 2020 +0200
    
        use animations (kinda broken)
    
    commit 7f25148c9359b30f491448b21cb6240d4f202c30
    Author: Yohaï-Eliel Berreby <yberreby@protonmail.ch>
    Date:   Wed Jun 24 15:33:11 2020 +0200
    
        get the player to face the correct direction
    
    commit 3afc8479b775a41abafc493b0bfd65aafdcab2c6
    Author: Yohaï-Eliel Berreby <yberreby@protonmail.ch>
    Date:   Wed Jun 24 15:23:10 2020 +0200
    
        use correct palette
    
    commit 6bba459e9408d1174624904e566a0f63b2b37870
    Author: Yohaï-Eliel Berreby <yberreby@protonmail.ch>
    Date:   Wed Jun 24 15:08:40 2020 +0200
    
        devlog: add day 4 scaffolding w/ gif
    
    commit 82a14b60f3876feb9df1c076d10046f0ae35910f
    Author: Yohaï-Eliel Berreby <yberreby@protonmail.ch>
    Date:   Wed Jun 24 14:53:33 2020 +0200
    
        wip
    
    commit ec7993d10f7624ab0da0907de900f05ffd85aae0
    Author: Yohaï-Eliel Berreby <yberreby@protonmail.ch>
    Date:   Wed Jun 24 14:37:23 2020 +0200
    
        move assets
    
    commit a03b61fadc32960a27a44d98810dc237331b1747
    Author: Yohaï-Eliel Berreby <yberreby@protonmail.ch>
    Date:   Wed Jun 24 14:35:24 2020 +0200
    
        begin switch from 16x16 to 8x16
    
    commit 723b9676b89b052d9033c56e0fbee10a4ffdd188
    Author: Yohaï-Eliel Berreby <yberreby@protonmail.ch>
    Date:   Wed Jun 24 14:30:05 2020 +0200
    
        rename project
    
    commit 8179ffad01a3fc896af1bc22ff6e5505fd1e280e
    Author: Yohaï-Eliel Berreby <yberreby@protonmail.ch>
    Date:   Wed Jun 24 13:40:10 2020 +0200
    
        remove useless files
    
    commit 78f2bd8420ab843ccb439ff12ea39f74c14b1933
    Author: Yohaï-Eliel Berreby <yberreby@protonmail.ch>
    Date:   Wed Jun 24 13:38:20 2020 +0200
    
        add gbdk submodule
    
    commit fcd5e9d94d3dc28bbf8cbcd484aa972d36f88bae
    Author: Yohaï-Eliel Berreby <yberreby@protonmail.ch>
    Date:   Wed Jun 24 13:35:06 2020 +0200
    
        add movement test subdir
    
    commit b7cadad8860b65c6909b5436400e921c6b702f19
    Author: Yohaï-Eliel Berreby <yberreby@protonmail.ch>
    Date:   Wed Jun 24 13:34:48 2020 +0200
    
        extend gitignore
    
    commit 25316c3bfe91969699847653cd76fe4863962c67
    Author: Yohaï-Eliel Berreby <yberreby@protonmail.ch>
    Date:   Wed Jun 24 13:33:08 2020 +0200
    
        wip: add shitty unfinished pong
    
    commit 15b95f5fb464b71bd9b572d1e5792d8266acc824
    Author: Yohaï-Eliel Berreby <yberreby@protonmail.ch>
    Date:   Wed Jun 24 13:32:07 2020 +0200
    
        wip: add assets

---
+++ movement-test/README.md
@@ -0,0 +1,3 @@
+Please build the `gdbk-n` support library before trying to compile this.
+
+You must `make assets` at least once. See the Makefile for details.
---
+++ movement-test/src/main.c
@@ -0,0 +1,148 @@
+#include <gb/gb.h>
+
+#include "player.sprites.h"
+
+#define PLAYER_SPRITE_ID 0
+
+// Animations data for the player's sprite
+UINT8 PLAYER_SPRITE_ANIM[] = {
+// LEN   FLIP            TILES ID         DIRECTION
+   4,    0, 0, 1, 1,     0, 2,  0,  2,           // Down
+   4,    0, 0, 1, 1,     4, 6,  4,   6,   // Up
+   4,    0, 0, 0, 0,     8, 10, 8, 10,   // Right
+   4,    1, 1, 1, 1,     8, 10, 8, 10,   // Left
+};
+
+// Offset of the animation of each direction in the global animation data
+#define PLAYER_DIRECTION_DOWN  0
+#define PLAYER_DIRECTION_UP    9
+#define PLAYER_DIRECTION_RIGHT 18
+#define PLAYER_DIRECTION_LEFT  27
+
+// Variables containing player state
+UINT8 player_x;
+UINT8 player_y;
+UINT8 player_direction;
+UINT8 player_animation_frame;
+UINT8 is_player_walking;
+
+// Flip the given sprite on X axis.
+//
+// sprite_id: the id ("nb") of the sprite to update.
+void flip_sprite_horiz(UINT8 sprite_id) {
+    set_sprite_prop(sprite_id, get_sprite_prop(sprite_id) | S_FLIPX);
+}
+
+// Remove the flip the given sprite on X axis.
+//
+// sprite_id: the id ("nb") of the sprite to update.
+void unflip_sprite_horiz(UINT8 sprite_id) {
+    set_sprite_prop(sprite_id, get_sprite_prop(sprite_id) & ~S_FLIPX);
+}
+
+// Update the tiles of the sprite to animate it.
+//
+// sprite_id: the id ("nb") of the sprite to update
+// anim:      pointer to the animation data
+// direction: direction of the animation (= offset of the animation in the global animation data)
+// frame:     the new frame of the animation that will be displayed
+//
+// Returns the next frame of the animation.
+UINT8 update_sprite_animation(UINT8 sprite_id, UINT8 *anim, UINT8 direction, UINT8 frame) {
+    UINT8 len = anim[direction];
+    UINT8 flip = anim[direction + 1 + frame]; // +1 : skip length
+    UINT8 tile_id = anim[direction + 1 + len + frame]; // +5: skip length+flip data
+
+    if (flip) {
+        flip_sprite_horiz(sprite_id);
+    } else {
+        unflip_sprite_horiz(sprite_id);
+    }
+
+    set_sprite_tile(sprite_id, tile_id);
+
+    return (frame + 1) % len;
+}
+
+void main(void) {
+    UINT8 keys = 0;
+    UINT8 frame_skip = 8;  // Update player's animation every 8 frame to
+                           // slow down the animation (8 frames = ~133 ms
+                           // between each animation frames)
+
+    // Initialize player's state
+    player_x = 80;
+    player_y = 72;
+    player_direction = PLAYER_DIRECTION_DOWN;
+    player_animation_frame = 0;
+    is_player_walking = 0;
+
+    // Load sprites' tiles in video memory
+    set_sprite_data(0, PLAYER_SPRITES_TILE_COUNT, PLAYER_SPRITES);
+
+    // Use 8x16 sprites
+    SPRITES_8x16;
+
+    // Makes sprites "layer" visible
+    SHOW_SPRITES;
+
+    // Init the sprite used for the player
+    move_sprite(PLAYER_SPRITE_ID, player_x, player_y);
+
+    // https://gbdev.gg8.se/wiki/articles/GBDK_set_sprite_prop
+    set_sprite_prop(PLAYER_SPRITE_ID, 0x00U);
+
+
+    while (1) {
+        // Wait for v-blank (screen refresh)
+        wait_vbl_done();
+
+        // Read joypad keys to know if the player is walking
+        // and in which direction
+        keys = joypad();
+        if (keys & J_UP) {
+            player_direction = PLAYER_DIRECTION_UP;
+            is_player_walking = 1;
+        } else if (keys & J_DOWN) {
+            player_direction = PLAYER_DIRECTION_DOWN;
+            is_player_walking = 1;
+        } else if (keys & J_LEFT) {
+            player_direction = PLAYER_DIRECTION_LEFT;
+            is_player_walking = 1;
+        } else if (keys & J_RIGHT) {
+            player_direction = PLAYER_DIRECTION_RIGHT;
+            is_player_walking = 1;
+        } else {
+            is_player_walking = 0;
+            frame_skip = 1;  // Force refresh of the animation frame
+        }
+
+        // Update the player position if it is walking
+        if (is_player_walking) {
+            if (player_direction == PLAYER_DIRECTION_RIGHT) player_x += 1;
+            else if (player_direction == PLAYER_DIRECTION_LEFT) player_x -= 1;
+            else if (player_direction == PLAYER_DIRECTION_UP) player_y -= 1;
+            else if (player_direction == PLAYER_DIRECTION_DOWN) player_y += 1;
+            move_sprite(PLAYER_SPRITE_ID, player_x, player_y);
+
+            // We do not update the animation on each frame: the animation
+            // will be too quick. So we skip frames
+            frame_skip -= 1;
+            if (!frame_skip) {
+                frame_skip = 8;
+            } else {
+                continue;
+            }
+        } else {
+            player_animation_frame = 0;
+        }
+
+
+        // Update sprites' tiles
+        player_animation_frame = update_sprite_animation(
+                PLAYER_SPRITE_ID,
+                PLAYER_SPRITE_ANIM,
+                player_direction,
+                player_animation_frame);
+    }
+}
