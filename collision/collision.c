5779e9e538d63fe5210e31e1cfd7521009ce4bc0
paulVezia <paul@LAPTOP-47NHQAM4.localdomain>
2020-06-25 19:28:46 +0200

    use of typedef for a cleaner code

--- collision/collision.c
+++ collision/collision.c
@@ -1,13 +1,14 @@
 #include <gb/gb.h>
 
-struct Rectangle {
-    UINT8 x;  // member variable
-    UINT8 y; // member variable
+typedef struct{
+    UINT8 x;
+    UINT8 y; 
     UINT8 width;
     UINT8 heigth;
-};
+} RECTANGLE;
 
-bool collision(Rectangle rect1, Rectangle rect2){
+//return true if there is a collision betwenn the two rectangles taken in argument
+bool collision(RECTANGLE rect1, RECTANGLE rect2){
     if (rect1.x < rect2.x + rect2.width &&
         rect1.x + rect1.width > rect2.x &&
         rect1.y < rect2.y + rect2.height &&
