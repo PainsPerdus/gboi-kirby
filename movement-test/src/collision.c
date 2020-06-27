0a70a2d6f8350ca98b6e07f1b682393e608eafbd
ChatPion <chatpion@gmail.com>
2020-06-27 15:57:52 +0200

    collision: add rect/rect penetration computation

--- movement-test/src/collision.c
+++ movement-test/src/collision.c
@@ -9,4 +9,29 @@ BOOLEAN rect_rect_collision(RECTANGLE* rect1, RECTANGLE* rect2) {
     && rect1->size.h + rect1->pos.y > rect2->pos.y;
 }
 
+void rect_rect_penetration(VEC* old_pos, VEC* new_pos, SIZE* size, RECTANGLE* obstacle, COLLISION_DIFF* diff) {
+    diff->dx = 0;
+    diff->dy = 0;
+    
+    // player from left
+    if (old_pos->x + size->w <= obstacle->pos.x 
+        && obstacle->pos.x < new_pos->x + size->w)
+        diff->dx = obstacle->pos.x - (new_pos->x - size->w);
 
+    // player from top
+    if (old_pos->y + size->h <= obstacle->pos.y
+        && obstacle->pos.y < new_pos->y + size->h)
+        diff->dy = obstacle->pos.y - (new_pos->y - size->h);
+
+    // player from right
+    if (old_pos->x >= obstacle->pos.x + obstacle->size.w
+        && obstacle->pos.x + obstacle->size.w >= new_pos->x)
+        diff->dx = obstacle->pos.x + obstacle->size.w - new_pos->x;
+
+    // player from bottom
+    if (old_pos->y >= obstacle->pos.y + obstacle->size.h
+        && obstacle->pos.y + obstacle->size.h >= new_pos->y)
+        diff->dy = obstacle->pos.y + obstacle->size.h - new_pos->y;
+
+
+}
--- movement-test/src/collision.h
+++ movement-test/src/collision.h
@@ -25,6 +25,24 @@ typedef struct {
  * @param rect2 
  * @return BOOLEAN 
  */
-BOOLEAN rect_rect_collision(RECTANGLE rect1, RECTANGLE rect2);
+BOOLEAN rect_rect_collision(RECTANGLE* rect1, RECTANGLE* rect2);
+
+
+//// COLLISION POST-PROCESSING
+typedef struct {
+    INT8 dx;
+    INT8 dy;
+} COLLISION_DIFF;
+
+/**
+ * @brief Compute the overlap between \p entity and \p obstacle
+ * 
+ * @param old_pos 
+ * @param new_pos 
+ * @param size Size of the entity
+ * @param obstacle 
+ * @param diff diff + new_pos = collision free pos
+ */
+void rect_rect_penetration(VEC* old_pos, VEC* new_pos, SIZE* size, RECTANGLE* obstacle, COLLISION_DIFF* diff);
 
 #endif
