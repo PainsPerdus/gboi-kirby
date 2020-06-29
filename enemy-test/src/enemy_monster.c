91112db8770a19453508c43890716f407e41edda
Adame BF <adame.bf@gmail.com>
2020-06-29 16:50:41 +0200

    [AI/basic enemy] Numerous corrections: compiles properly, and almost works as intended. Known issue: right sprite doesn't show up.

--- enemy-test/src/enemy_monster.c
+++ enemy-test/src/enemy_monster.c
@@ -1,13 +1,12 @@
 #include <gb/gb.h>
 
-#define ENEMY_SPRITE_L_ID 0
-#define ENEMY_SPRITE_R_ID 1
+#include "dummy-tileset.h"
 
 #define ENEMY_ATTACK_PROJECTILE 0
 #define ENEMY_ATTACK_MELEE 1
 #define ENEMY_ATTACK_SELF 2 // used for teting purposes
 
-struct ENEMY {
+typedef struct enemy {
 	UINT8 enemy_sprite_l; // enemy left sprite in the tileset
 	UINT8 enemy_sprite_r; // enemy right sprite
 	UINT8 sprite_id; // sprite ID (0-39)
@@ -20,7 +19,7 @@ struct ENEMY {
 	// Unhandled at this point:
 	// - x/y monster velocity, or whatever way to handle monster speed (to be discussed: how to alter these when a collision occurs? I don't think we want monsters going back and forth along the same straight path all game long)
 	// - projectile speed (if we decide that it depends on the monster rather than, for example, on how far the player got in the game)
-}
+} ENEMY;
 
 // Temporary background tileset for testing purposes
 const UINT8 BKG_TILESET[] = {
@@ -29,18 +28,18 @@ const UINT8 BKG_TILESET[] = {
     0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
     // Tile 01: Block
     0xff, 0x01, 0x81, 0x7f, 0xbd, 0x7f, 0xa5, 0x7b,
-    0xa5, 0x7b, 0xbd, 0x63, 0x81, 0x7f, 0xff, 0xff,
+    0xa5, 0x7b, 0xbd, 0x63, 0x81, 0x7f, 0xff, 0xff
 };
 
 // TO BE CHANGED: Temporary sprites tileset for testing purposes
-const UINT8 SPRITE_TILESET[] = {
+/* const UINT8 SPRITE_TILESET[] = {
 	// Tile 00: will be temporarily used as a placeholder for the projectile
     0x7e, 0x00, 0x81, 0x7f, 0x81, 0x7f, 0x81, 0x7f,
     0x81, 0x7f, 0x81, 0x7f, 0x81, 0x7f, 0x7e, 0x7e,
     // Tile 01: Cherry - temporary placeholder for the projectile
     0x04, 0x04, 0x04, 0x04, 0x0a, 0x0a, 0x12, 0x12,
     0x66, 0x00, 0x99, 0x77, 0x99, 0x77, 0x66, 0x66,
-}
+}*/
 
 // Temporary tilemap for testing purposes
 const UINT8 BKG_TILEMAP[] = {
@@ -61,26 +60,24 @@ const UINT8 BKG_TILEMAP[] = {
     1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
     1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
     1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
-    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1
 };
 
 // Initialize an enemy unit
-void init_enemy(ENEMY *unit, UINT8 enemy_sprite_l, UINT8 enemy_sprite_r, UINT8 s_id, UINT8 attack_type, UINT8 damage, UINT8 hp, UINT8 fba)
-{
+void init_enemy(ENEMY *unit, UINT8 enemy_sprite_l, UINT8 enemy_sprite_r, UINT8 s_id, UINT8 attack_type, UINT8 damage, UINT8 hp, UINT8 fba) {
 	unit->enemy_sprite_l = enemy_sprite_l;
 	unit->enemy_sprite_r = enemy_sprite_r;
 	unit->sprite_id = s_id;
 	unit->attack_type = attack_type;
 	unit->max_health = hp;
-	unit->health = hp; // Enemies always start with 100% HP.
+	unit->health = hp; // Enemies always start full HP.
 	unit->damage = damage;
 	unit->frames_between_attacks = fba;
 	unit->frames_until_next_attack = fba; // There is one full cycle before the enemy starts behaving normally. Might want to be able to configure that.
 }
 
 // (To be tested) Display enemy unit on-screen, with specified sprite number (0-39), at specified x and y coordinates.
-void display_enemy(ENEMY *unit, UINT8 xpos, UINT ypos)
-{
+void display_enemy(ENEMY *unit, UINT8 xpos, UINT8 ypos) {
 	// Initialize left sprite
 	set_sprite_tile(unit->sprite_id, unit->enemy_sprite_l);
 	move_sprite(unit->sprite_id, xpos, ypos);
@@ -91,8 +88,7 @@ void display_enemy(ENEMY *unit, UINT8 xpos, UINT ypos)
 }
 
 // Play death sequence (blinking, presumably), then make the enemy disappear
-void enemy_death(ENEMY *unit)
-{
+void enemy_death(ENEMY *unit) {
 	// Blinking animation - disabled for now
 	/*
 	UINT8 blinking_cycles = 0;
@@ -118,8 +114,7 @@ void enemy_death(ENEMY *unit)
 }
 
 // Enemy unit loses specified amount of HP
-void enemy_hp_loss(ENEMY *unit, UINT8 amount)
-{
+void enemy_hp_loss(ENEMY *unit, UINT8 amount) {
 	if (amount < unit->health) // The unit survives, so it just loses the specified amount of HP
 	{
 		unit->health -= amount;
@@ -128,13 +123,12 @@ void enemy_hp_loss(ENEMY *unit, UINT8 amount)
 	else // The unit dies: bring its HP to 0 and then call enemy_death
 	{
 		unit->health = 0;
-		enemy_death(*unit);
+		enemy_death(unit);
 	}
 }
 
 // Enemy unit gains specified amount of HP. This is capped to max enemy health. (To be tested.)
-void enemy_hp_regen(ENEMY *unit, UINT8 amount)
-{
+void enemy_hp_regen(ENEMY *unit, UINT8 amount) {
 	if (unit->health + amount <= unit->max_health) // Gain intended amount
 	{
 		unit->health += amount;
@@ -146,14 +140,13 @@ void enemy_hp_regen(ENEMY *unit, UINT8 amount)
 }
 
 // Enemy unit launches its attack #TODO: handle MELEE/PROJECTILE
-void enemy_attack(ENEMY *unit)
-{
+void enemy_attack(ENEMY *unit) {
 	if (unit->health) // Check: an enemy can only attack if it has nonzero health
 	{
 		switch (unit->attack_type)
 		{
 			case ENEMY_ATTACK_SELF:
-				enemy_hp_loss(*unit, unit->damage);
+				enemy_hp_loss(unit, unit->damage);
 				// Optional: might want to load sound!
 				break;
 			
@@ -166,30 +159,27 @@ void enemy_attack(ENEMY *unit)
 	}
 }
 
-void main(void)
-{
+void main(void) {
     set_bkg_data(0, 2, BKG_TILESET);
     set_bkg_tiles(0, 0, 20, 18, BKG_TILEMAP);
-	set_sprite_data(0, 2, SPRITE_TILESET);
+	set_sprite_data(0, 4, DUMMY_SPRITES);
 	ENEMY basic;
 
-	// Loading self-attacking enemy (two attacks/second) #TODO: MISSING ACTUAL SPRITES TILESET
-	init_enemy(*basic, 1, 1, ENEMY_ATTACK_SELF, 13, 2, 30);
-	display_enemy(*basic,1,72,80);
+	// Loading self-attacking enemy (two attacks/second)
+	init_enemy(&basic, 0, 2, 0, ENEMY_ATTACK_SELF, 13, 2, 30);
+	display_enemy(&basic, 72, 80);
 	SHOW_BKG;
-	//SPRITES_8x16; // #TODO: Off until I changed to a proper 8x16 tileset
+	SPRITES_8x16;
 	SHOW_SPRITES;
 	
-	while(1)
-	{
+	while(1) {
 		wait_vbl_done(); // Wait screen refresh
 		
 		// Proceed by one frame and check whether the enemy can attack
 		basic.frames_until_next_attack--;
-		if (!frames_until_next_attack)
-		{
+		if (!basic.frames_until_next_attack) {
 			// Attack and reset frame countdown
-			enemy_attack(*basic);
+			enemy_attack(&basic);
 			basic.frames_until_next_attack = basic.frames_between_attacks;
 		}
 	}
