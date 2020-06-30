c1994b44846b8c8c14cf4f159558ed2d555cf647
Adame BF <adame.bf@gmail.com>
2020-06-30 16:57:32 +0200

    [projectiles] Implement init and display functions for pprojectiles.

--- projectile/src/projectile.c
+++ projectile/src/projectile.c
@@ -1,2 +1,26 @@
 #include <gb/gb.h>
 
+#include "projectile.h"
+
+// Initialize projectile data
+void projectile_init(PROJECTILE* p, UINT8 s_id, UINT8 s_tile, UINT8 targets, UINT8 damage, UINT8 velocity) {
+	p->sprite_id = s_id;
+	p->sprite_tile = s_tile;
+	p->targets = targets;
+	p->velocity = velocity;
+	p->damage = damage;
+}
+
+// Place projectile at specified (x,y) coordinates, and define its direction (see FIRING_DIRECTION_* macros)
+void projectile_display(PROJECTILE* p, UINT8 xpos, UINT8 ypos, UINT8 direction) {
+	// Store the values
+	p->firing_direction = direction;
+	p->xpos = xpos;
+	p->ypos = ypos;
+	
+	// Initialize projectile sprite
+	set_sprite_tile(p->sprite_id, p->sprite_tile);
+	
+	// Place the sprite
+	move_sprite(p->sprite_id, p->xpos + X_SPRITE_OFFSET, p->ypos + Y_SPRITE_OFFSET);
+}
--- projectile/src/projectile.h
+++ projectile/src/projectile.h
@@ -12,15 +12,41 @@
 #define TARGET_MONSTER 1
 #define TARGET_ANY 2
 
+#ifndef _SPRITE_OFFSET
+#define X_SPRITE_OFFSET 8
+#define Y_SPRITE_OFFSET 16
+#endif
+
 typedef struct projectile {
 	UINT8 sprite_tile; // sprite ID in the tileset
 	UINT8 sprite_id; // sprite ID (0-39)
 	UINT8 firing_direction; // see FIRING_DIRECTION_* macros
 	UINT8 xpos;
 	UINT8 ypos;
-	UINT8 velocity;
+	UINT8 velocity; // projectile movement speed in pixels per frame
 	UINT8 targets; // see TARGET_* macros
-	UINT8 damage;
+	UINT8 damage; // damage the projectile should do. This will depend on the unit firing it.
 } PROJECTILE;
 
+/**
+ * @brief Initializes a projectile
+ *
+ * @param p projectile
+ * @param s_id sprite ID (0-39)
+ * @param s_tile projectile tile
+ * @param targets
+ * @param damage
+ * @param velocity
+ */
+void projectile_init(PROJECTILE* p, UINT8 s_id, UINT8 s_tile, UINT8 targets, UINT8 damage, UINT8 velocity);
+
+/**
+ * @brief Place projectile at specified (x,y) coordinates, and define its direction (see FIRING_DIRECTION_* macros)
+ *
+ * @param p
+ * @param xpos
+ * @param ypos
+ * @param direction see FIRING_DIRECTION_* macros
+ */
+void projectile_display(PROJECTILE* p, UINT8 xpos, UINT8 ypos, UINT8 direction);
 #endif
