18a15e6f406ae0b36706812a545421dd4b17fee2
Yohaï-Eliel BERREBY <yberreby@protonmail.ch>
2020-07-02 21:39:44 +0200

    provide top-level build.sh

--- build.sh
+++ build.sh
@@ -3,6 +3,13 @@
 # YES, this is awful, and this point we have no time to care.
 # If someone wants to fix the Makefile, be my guest...
 
+
+git submodule update --init
+
+cd gbdk-2020
+make
+
+cd ../main-rom
 make clean
 make assets
 make || true
